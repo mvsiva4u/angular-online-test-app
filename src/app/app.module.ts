f712d11f0eedfb31b9f70a2bab8bc5c4434e7e09
mvsiva4u <mvsiva4u@gmail.com>
2020-12-13 22:06:49 +0530

    Angulat online test application
    Online test APP initital commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MatRadioModule, MAT_RADIO_DEFAULT_OPTIONS } from '@angular/material/radio';
+import { AppComponent } from './app.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { AppRoutingModule } from './app-routing.module';
+import { IntroductionComponent } from './containers/introduction/introduction.component';
+import { QuestionComponent } from './containers/question/question.component';
+import * as QuestionComponent2 from './components/question/question.component';
+import { ResultsComponent } from './containers/results/results.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    IntroductionComponent,
+    QuestionComponent,
+    QuestionComponent2.QuestionComponent,
+    ResultsComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatRadioModule,
+    MatIconModule,
+    MatButtonModule,
+    NgbModule,
+    AccordionModule.forRoot()
+  ],
+  providers: [{
+    provide: MAT_RADIO_DEFAULT_OPTIONS,
+    useValue: { color: 'accent' },
+  }],
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+    NO_ERRORS_SCHEMA
+  ]
+})
+export class AppModule { }
---
+++ src/app/containers/introduction/introduction.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'quiz-intro',
+  templateUrl: './introduction.component.html',
+  styleUrls: ['./introduction.component.scss']
+})
+export class IntroductionComponent {
+
+  constructor(private router: Router) {}
+
+  startQuiz() {
+    this.router.navigateByUrl('/question/1');
+  }
+}
